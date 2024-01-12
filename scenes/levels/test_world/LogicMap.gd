6c71370d26105c5bdd145d40644e555fa5a4e85f
XadosIV <jxds.lph@pm.me>
2024-01-12 11:11:02 +0100

    LES ACTEURS FONCTIONNELS

--- scenes/levels/test_world/LogicMap.gd
+++ scenes/levels/test_world/LogicMap.gd
@@ -2,12 +2,17 @@ class_name LogicMap extends TileMap
 
 var posPlayer : Vector2
 
+var actors : Dictionary #<Vector2, Node2D>
+
+
 func _ready() -> void:
 	load_map()
 	pass
 
 func load_map() -> void: #pour plus tard
-	pass
+	for child in get_children():
+		actors[str(local_to_map(child.position))] = child
+	print(actors)
 
 func init() -> void: #Stocke la position du player par rapport à la tilemap
 	posPlayer = local_to_map(get_parent().get_node("Player").position)
@@ -23,8 +28,11 @@ func playerMove(movement) -> bool: # renvoie si le mouvement a pu être fait ou 
 
 func interact(movement) -> bool:
 	var atlasCoord = get_cell_atlas_coords(0, posPlayer + movement)
-	if atlasCoord == Vector2i(1,0):
+	if atlasCoord == Vector2i(1,0): #Item
 		set_cell(0, posPlayer+movement)
+		return true
+	elif atlasCoord == Vector2i(2,0):
+		actors[str(posPlayer+movement)].interact()
 		return true
 	return false
 
