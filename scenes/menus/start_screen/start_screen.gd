ad312fefe7e7b03e0f464ec0a6b95b40fb24f9f4
Djapamal <al.leonarduzzi@gmail.com>
2024-02-15 10:29:23 +0100

    V1 drag and drop

--- scenes/menus/start_screen/start_screen.gd
+++ scenes/menus/start_screen/start_screen.gd
@@ -4,11 +4,16 @@ extends Control
 
 var in_transition : bool = false
 
+func _ready():
+	get_viewport().files_dropped.connect(on_files_dropped)
+
+func on_files_dropped(files):
+	print(files)
 
 func _on_button_button_up():
-	SceneManager.load_new_scene("res://scenes/levels/test_world/starting_village.tscn","fade_to_black")
+	SceneManager.load_new_scene("res://scenes/levels/test_world/starting_village.tscn", "wipe_to_right")
 	
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("interact") and not in_transition:
 		in_transition = true
-		SceneManager.load_new_scene("res://scenes/levels/test_world/starting_village.tscn","fade_to_black")
+		SceneManager.load_new_scene("res://scenes/levels/test_world/starting_village.tscn","wipe_to_right")
