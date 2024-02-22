16afeadca84fc969c60ca7a01fac7a7a30e0324f
XadosIV <83866570+XadosIV@users.noreply.github.com>
2024-02-22 12:27:27 +0100

    Fix nouveau format de json

--- autoloads/dialog_manager/dialog_manager.gd
+++ autoloads/dialog_manager/dialog_manager.gd
@@ -19,11 +19,9 @@ func get_dialog(str: String):
 	return dialog[str]
 
 func create_dialog(nom: String, data:Array):
-	print(sm.json_data["exercises"][1])
 	if not dialog_exists(nom):
 		for i in data:
-			if i["type"] == "qcm":
-				i["type"] = "mcq"
+			if i["type"] == "mcq":
 				i["text"] = sm.json_data["exercises"][i["id"]]["question"]
 				i["questions"] = sm.json_data["exercises"][i["id"]]["options"]
 		print(data)
