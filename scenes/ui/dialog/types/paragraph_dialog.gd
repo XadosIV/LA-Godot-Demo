bb1835984a13b96f6a07fe9c11253a828d8512d2
Djapamal <al.leonarduzzi@gmail.com>
2024-03-20 13:32:34 +0100

    Rework de dialog_box

---
+++ scenes/ui/dialog/types/paragraph_dialog.gd
@@ -0,0 +1,8 @@
+class_name ParagraphDialog extends Node
+
+var npc_name : String = "John Doe"
+var text : String = "..."
+
+func init(npc_name : String, text : String):
+	self.npc_name = npc_name
+	self.text = text
