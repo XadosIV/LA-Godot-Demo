aeb5ce4bb1fbc3d1bd57cb5d1fdcfd8c12c279a4
Djapamal <al.leonarduzzi@gmail.com>
2024-05-22 19:35:20 +0200

    fix tileset 4 & prep pour la soutenance

--- scenes/menus/start_screen/start_screen.gd
+++ scenes/menus/start_screen/start_screen.gd
@@ -3,13 +3,20 @@ extends Control
 # TODO Créer un autoload avec toutes les varaibles vers les scènes
 
 var in_transition : bool = false
-var path
+var path = "res://SCENARIO_PRESENTATION.json"
 
 @onready var actionsManager : ActionsManager = get_node("/root/ActionManager")
 
 func _ready():
 	get_viewport().files_dropped.connect(on_files_dropped)
-	$VBoxContainer/Button.visible = false
+	
+	# Pour charger le scénario dans le cadre de la soutenance
+	#$VBoxContainer/Button.visible = false
+	if (path == null):
+		$VBoxContainer/Button.visible = false
+	else:
+		actionsManager.read_json(path)
+		$Label.text = "Scénario pour la soutenance"
 
 func on_files_dropped(files):
 	path = files[0]
