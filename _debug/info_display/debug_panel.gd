0bff061bd6acaad2e7bd4062af27d4f1249adcf8
Djapamal <al.leonarduzzi@gmail.com>
2024-05-22 20:34:57 +0200

    fix debug

--- _debug/info_display/debug_panel.gd
+++ _debug/info_display/debug_panel.gd
@@ -12,13 +12,16 @@ var property_current_scene
 var property_time
 var property_show_logic_map
 
-var show_logic_map: bool = false
 var frames_per_second : String
 var position_x: int
 var position_y: int
 
 func _ready() -> void:
-	visible = false
+	visible = gm.show_debug
+	if gm.show_logic_map:
+		logic_map.show_map()
+	else:
+		logic_map.hide_map()
 	add_fps_debug_property()
 	add_position_debug_property()
 	add_current_scene_debug_property()
@@ -26,16 +29,17 @@ func _ready() -> void:
 	add_time_debug_property()
 
 func _input(event) -> void:
-	if visible and  event.is_action_pressed("L_key"):
-		show_logic_map = !show_logic_map
-		if show_logic_map:
+	if visible and event.is_action_pressed("L_key"):
+		gm.show_logic_map = !gm.show_logic_map
+		if gm.show_logic_map:
 			logic_map.show_map()
 		else:
 			logic_map.hide_map()
 	elif event.is_action_pressed("debug"):
 		visible = !visible
-		if show_logic_map and !visible:
-			show_logic_map = !show_logic_map
+		gm.show_debug = !gm.show_debug
+		if gm.show_logic_map and !visible:
+			gm.show_logic_map = false
 			logic_map.hide_map()
 			
 
@@ -51,7 +55,7 @@ func _process(delta) -> void:
 		property_position.text = "X: " + str((position_x+8)/16) + " ; Y: " + str((position_y+8)/16)
 		
 		# Update logic map visibility
-		property_show_logic_map.text = "show_logic_map: "+str(show_logic_map)
+		property_show_logic_map.text = "show_logic_map: "+str(gm.show_logic_map)
 		
 		#Update time
 		property_time.text = "time: "+str(gm.time)+"\nhour: "+str(gm.hour)+":"+str(gm.minute)
@@ -74,7 +78,7 @@ func add_current_scene_debug_property() -> void:
 func add_show_logic_map_ebug_property() -> void:
 	property_show_logic_map = Label.new()
 	property_container.add_child(property_show_logic_map)
-	property_show_logic_map.text = "show_logic_map: "+str(show_logic_map)
+	property_show_logic_map.text = "show_logic_map: "+str(gm.show_logic_map)
 
 func add_time_debug_property() -> void:
 	property_time = Label.new()
--- autoloads/game_manager/game_manager.gd
+++ autoloads/game_manager/game_manager.gd
@@ -18,6 +18,10 @@ var day = 0
 var hour = 0
 var minute = 0
 
+#Debug
+var show_logic_map: bool = false
+var show_debug: bool = false
+
 func _ready():
 	time = INGAME_TO_REAL_MINUTE_DURATION *initialHour *MINUTES_PER_HOURS
 
